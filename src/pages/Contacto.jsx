63d1ef9d51260d2afed46bede51919a7ad91979b
Rúbel Maneiro <72143708+rmaneiro28@users.noreply.github.com>
2024-11-01 23:11:07 -0400

    update contact with map

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -17,23 +17,23 @@ export default function Contacto() {
                             <div className="space-y-4">
                                 
                                 <div className="flex items-center">
-                                    <FontAwesomeIcon icon={faMapMarkerAlt} className="h-6 w-6 text-blue-500 mr-2" />
-                                    <span>Av. Rómulo Gallegos Centro Juvenil Don Bosco, piso 1, Boleíta Norte. Municipio Sucre, Estado Miranda.</span>
+                                    <FontAwesomeIcon icon={faMapMarkerAlt} className="h-6 w-6 text-blue mr-2" />
+                                    <span className="w-20">Av. Rómulo Gallegos Centro Juvenil Don Bosco, piso 1, Boleíta Norte. Municipio Sucre, Estado Miranda.</span>
                                 </div>
                                 <div className="flex items-center">
-                                    <FontAwesomeIcon icon={faPhone} className="h-6 w-6 text-blue-500 mr-2" />
+                                    <FontAwesomeIcon icon={faPhone} className="h-6 w-6 text-blue mr-2" />
                                     <div className="flex flex-col">
                                         <p>+58 212-234-5587</p>
                                         <p>+58 424-206-9080</p>
                                     </div>
                                 </div>
                                 <div className="flex items-center">
-                                    <FontAwesomeIcon icon={faIdCard} className="h-6 w-6 text-blue-500 mr-2" />
+                                    <FontAwesomeIcon icon={faIdCard} className="h-6 w-6 text-blue mr-2" />
                                     <span className="mr-2 font-medium">RIF: </span>
                                     <span>J-30234355-0</span>
                                 </div>
                                 <div className="flex items-center">
-                                    <FontAwesomeIcon icon={faMailBulk} className="h-6 w-6 text-blue-500 mr-2" />
+                                    <FontAwesomeIcon icon={faMailBulk} className="h-6 w-6 text-blue mr-2" />
                                     <a href="mailto:juventudytrabajobup@gmail.com" className="mr-2 text-blue" >juventudytrabajobup@gmail.com</a>
                                 </div>
                             </div>
@@ -45,7 +45,7 @@ export default function Contacto() {
 
                         <div>
                             <div className="flex items-center">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d561.1501475396168!2d-66.82522768352437!3d10.493514491899063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2a590072e9c66b%3A0xc89409db48c9189f!2sCentro%20de%20Capacitaci%C3%B3n%20Laboral%20Barrios%20unidos%20de%20Petare!5e1!3m2!1ses-419!2sus!4v1730515293680!5m2!1ses-419!2sus" className='w-[600px] h-[450px] b-0 rounded-md' allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d561.1501475396168!2d-66.82522768352437!3d10.493514491899063!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2a590072e9c66b%3A0xc89409db48c9189f!2sCentro%20de%20Capacitaci%C3%B3n%20Laboral%20Barrios%20unidos%20de%20Petare!5e1!3m2!1ses-419!2sus!4v1730515293680!5m2!1ses-419!2sus" className='md:w-[600px] md:h-[450px] max-md:h-[400px] max-md:h-[250px] b-0 rounded-md' allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
                             </div>
                         </div>
                     </div>
